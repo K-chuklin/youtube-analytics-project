711c38fc0bb38553d7df817c7e6646e4f222bde7
chuklin <inkarttatoo@gmail.com>
2023-08-22 23:01:14 +0500

    Edits in homework_4

--- src/video.py
+++ src/video.py
@@ -20,17 +20,20 @@ class Video:
         self.view_count = video_response['items'][0]['statistics']['viewCount']  # Кол-во просмотров
         self.count_likes = video_response['items'][0]['statistics']['likeCount']  # Кол-во лайков
 
+    def __str__(self):
+        return self.video_title
+
 
 class PLVideo:
     def __init__(self, video_id: str, playlist_id: str):
         self.__video_id = video_id  # id видео;
         video_response = youtube.videos().list(id=self.__video_id,
                                                part='snippet,statistics,contentDetails,topicDetails').execute()
-        # playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-        #                                      part='contentDetails', maxResults=50,).execute()
-        # print(playlist_videos)
         self.video_title = video_response['items'][0]['snippet']['title']  # Название видео
         self.url = 'https://youtu.be/' + playlist_id + '/' + video_id  # Ссылка на видео
         self.view_count = video_response['items'][0]['statistics']['viewCount']  # Кол-во просмотров
         self.count_likes = video_response['items'][0]['statistics']['likeCount']  # Кол-во лайков
         self.__playlist_id = playlist_id
+
+    def __str__(self):
+        return self.video_title
